c91eec3f0cf8a8c1f8f79172dfd54c9684cd935b
Usman Imran <usmanimran4121812@gmail.com>
2025-11-05 00:07:10 +0500

    Fix case-sensitive import for Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home.jsx'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
